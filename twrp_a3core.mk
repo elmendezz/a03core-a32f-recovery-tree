64a854b3b15e3223255be1bc7ead8f9524e7bd2b
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-02-20 13:45:50 -0800

    Update twrp_a3core.mk

--- twrp_a3core.mk
+++ twrp_a3core.mk
@@ -4,11 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from SRPUH09A006 device
 $(call inherit-product, device/samsung/a3core/device.mk)
@@ -25,4 +24,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="a3corexx-user 11 RP1A.201005.001 A032FXXS6CXE1 release-keys"
 
 BUILD_FINGERPRINT := 
-
